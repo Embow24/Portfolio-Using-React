e306b489d7709be2dab65320fdf278504ba148e7
Emily Kent <kentembowemily@gmail.com>
2024-03-02 19:46:54 +0000

    created Navbar function and imported to App.jsx

---
+++ Portfolio-Using-React/src/Components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function Navbar() {
+return(
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Project Gallery</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+)
+}
+
+export default Navbar
