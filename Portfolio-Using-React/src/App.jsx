d96f6e8764f970d5d59cd365f41b370484cafceb
Emily Kent <kentembowemily@gmail.com>
2024-03-03 20:16:42 +0000

    Updated route path so home page is the landing page

--- Portfolio-Using-React/src/App.jsx
+++ Portfolio-Using-React/src/App.jsx
@@ -12,6 +12,7 @@ function App() {
       <div>
         <Navbar />
         <Routes>
+        <Route path='/*' element={<Home/>} />
           <Route path='/home' element={<Home/>} />
           <Route path='/projects' element={<Projects/>} />
           <Route path='/contact' element={<Contact/>} />
