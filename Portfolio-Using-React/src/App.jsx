3e4b8f371081c2fc590b047d2ff8ea21294792f6
Emily Kent <kentembowemily@gmail.com>
2024-03-03 14:04:27 +0000

    imported and added router, routes and route for the home page

--- Portfolio-Using-React/src/App.jsx
+++ Portfolio-Using-React/src/App.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
+import {BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Navbar from './Components/Navbar/Navbar.jsx'
+import Home from './Pages/Home.jsx'
 import './App.css'
 
 function App() {
   return (
-    <>
+    <Router>
       <div>
         <Navbar />
+        <Routes>
+          <Route path='/home' element={<Home/>} />
+        </Routes>
       </div>
-    </>
+    </Router>
   )
 }
 
