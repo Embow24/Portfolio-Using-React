f20ebc418e71a0a3be7ab092034e729b8bdb45dc
Emily Kent <kentembowemily@gmail.com>
2024-03-04 13:36:34 +0000

    Created project.JSON file to store project data

--- Portfolio-Using-React/src/Pages/Projects.jsx
+++ Portfolio-Using-React/src/Pages/Projects.jsx
@@ -1,60 +1,82 @@
 import React from 'react'
+//import project from './Components/ProjectGallery/Project.JSON'
 
 function Projects() {
     return (
     <div>
-        <div className="row row-cols-1 row-cols-md-3 g-6">
-  <div className="col">
-    <div className="card h-100">
-    <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text"></p>
-    </div>
-    </div>
-  </div>
-  <div className="col">
-    <div className="card h-100">
-    <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text"></p>
-    </div>
-    </div>
-  </div>
-  <div className="col">
-    <div className="card h-100">
-    <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text"></p>
-    </div>
-    </div>
-  </div>
-  <div className="col">
-    <div className="card h-100">
-    <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text"></p>
-    </div>
-    </div>
-  </div>
-  <div className="col">
-    <div className="card h-100">
-    <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text"></p>
-    </div>
-    </div>
-  </div>
-  <div className="col">
-    <div className="card h-100">
-    <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text"></p>
-    </div>
-    </div>
-  </div>
-</div>
+        <ul class="list-group list-group-flush">
+  <li class="list-group-item">Work Day Planner</li>
+  <li class="list-group-item">Coding Quiz</li>
+  <li class="list-group-item">Password Generator</li>
+  <li class="list-group-item">Weather Dashboard</li>
+  <li class="list-group-item">Healthy Bites Group Project</li>
+  <li class="list-group-item">Team Profile Generator</li>
+</ul>
 </div>
     )
 }
 
 export default Projects
+
+/* <div className="row row-cols-1 row-cols-md-3 g-6">
+  <div className="col">
+    <div className="card h-100">
+    <div className="card-body">
+        <h5 className="card-title">Work Day Planner</h5>
+        <p className="card-text">Created using HTML, CSS, Javacript, JQuery and day.js</p>
+        <p>Deployed Version: </p>
+        <p>GitHub Repository: </p>
+    </div>
+    </div>
+  </div>
+  <div className="col">
+    <div className="card h-100">
+    <div className="card-body">
+        <h5 className="card-title">Coding Quiz</h5>
+        <p className="card-text">Created using HTML, CSS and JavaScript</p>
+        <p>Deployed Version: </p>
+        <p>GitHub Repository: </p>
+    </div>
+    </div>
+  </div>
+  <div className="col">
+    <div className="card h-100">
+    <div className="card-body">
+        <h5 className="card-title">Password Generator</h5>
+        <p className="card-text">Created using HTML, CSS and JavaScript</p>
+        <p>Deployed Version: </p>
+        <p>GitHub Repository: </p>
+    </div>
+    </div>
+  </div>
+  <div className="col">
+    <div className="card h-100">
+    <div className="card-body">
+        <h5 className="card-title">Weather Dashboard</h5>
+        <p className="card-text">Created using HTML, CSS, JavaScript, JQuery, day.js and open weather API</p>
+        <p>Deployed Version: </p>
+        <p>GitHub Repository: </p>
+    </div>
+    </div>
+  </div>
+  <div className="col">
+    <div className="card h-100">
+    <div className="card-body">
+        <h5 className="card-title">Healthy Bites Group Project</h5>
+        <p className="card-text">Created using HTML, CSS, JavaScript, JQuery, Spoonacular and Pexels APIs</p>
+        <p>Deployed Version: </p>
+        <p>GitHub Repository: </p>
+    </div>
+    </div>
+  </div>
+  <div className="col">
+    <div className="card h-100">
+    <div className="card-body">
+        <h5 className="card-title">Team Profile Generator</h5>
+        <p className="card-text">Created Using JavaScript, npm inquirer and npm jest to generate a HTML file</p>
+        <p>Deployed Version: </p>
+        <p>GitHub Repository: </p>
+    </div>
+    </div>
+  </div>
+</div> */
