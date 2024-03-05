2a164844de5525be0e219eaedc3e7ca124f4f8a5
Emily Kent <kentembowemily@gmail.com>
2024-03-05 14:20:10 +0000

    Added social media links to contact page

--- Portfolio-Using-React/src/Pages/Contact.jsx
+++ Portfolio-Using-React/src/Pages/Contact.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Form from '../Components/Form/form'
+import Socials from '../Components/Socials/socials'
 
 function Contact(){
     return(
         <div>
             <Form></Form>
+            <Socials></Socials>
         </div>
     )
 }
