a8b9268b0e60a6f53976567681cd13306312175a
Emily Kent <kentembowemily@gmail.com>
2024-03-05 15:07:59 +0000

    README file updated

--- Portfolio-Using-React/src/Components/ProjectGallery/Project.jsx
+++ Portfolio-Using-React/src/Components/ProjectGallery/Project.jsx
@@ -1,21 +1,21 @@
 
 
 export default function Project({item}){
-const displayCard = () => {
+const displayCard = (item) => {
 return (
     <div className="card" style="width: 18rem;">
   <div className="card-body">
     <h5 className="card-title">{item.projectName}</h5>
     <p className="card-text">{item.description}</p>
-    <a href="#" className="card-link">GitHub link</a>
-    <a href="#" className="card-link">Deployed link</a>
+    <a href={item.github} className="card-link">GitHub link</a>
+    <a href={item.deployed} className="card-link">Deployed link</a>
   </div>
 </div>
 )
 }
     return (
         <li className="list-group-item">
-            <button type="button" onClick={displayCard}>{item.projectName}</button>
+            <button type="button" onClick={displayCard}><a href={displayCard}>{item.projectName}</a></button>
           </li>
     )
 }
